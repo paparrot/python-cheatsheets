5ea0acb6e1600be7350e4a259bf40fcb7a17e399
Borteq2 <borteq2@yandex.ru>
2023-03-20 14:52:38 +0300

    add simple hint for sets add additional example for creating sets add useful example using sets for deleting duplicates add frozensets

--- basics/tuples_sets.py
+++ basics/tuples_sets.py
@@ -1,9 +1,9 @@
-# Tuple is a collection which is ordered and unchangeble. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
 
 # Create tuple
 fruits = ('Apples', 'Oranges', 'Grapes')
-# fruits = tuple(('Apples', 'Oranges', 'Grapes')) // Uncommon
-# fruits = ('Apple',) // Single value need trailing comma
+# fruits = tuple(('Apples', 'Oranges', 'Grapes')) # Uncommon
+# fruits = ('Apple',) # Single value need trailing comma
 print(type(fruits))
 print(fruits[1])
 
@@ -12,12 +12,13 @@ print(fruits[1])
 
 # Delete tuple
 del fruits
-# prints(fruits) // Not found
+# prints(fruits) # Not found
 
-# Set is a collection which is unordered and unindexed. No duplicate methods.
-
+# Set is a changeable collection which is unordered and unindexed. No duplicate methods. Only unique members.
 # Create set
 fruits_set = {'Apples', 'Oranges', 'Mango'}
+# collection = ('Apples', 'Oranges', 'Grapes')
+# fruits_set = set(collection)
 
 # Check if in set
 print('Apples' in fruits_set)
@@ -41,3 +42,15 @@ print(fruits_set)
 
 del fruits_set
 # print(fruits_set) // Not found
+
+# You can use sets for deleting duplicates from collections
+collection = ['1', '2', '1', '3', '1', '5', '7', '11', '1']
+print(set(collection))
+
+# frozenset is a collection like set, but unchangeable
+collection.append('999')
+print(collection)
+
+frozen_collection = frozenset(collection)
+# frozen_collection.append('1010')  # AttributeError
+# print(frozen_collection)
