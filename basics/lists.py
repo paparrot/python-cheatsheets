68092c6f58cb06872bc2e2de74eab3e67c27463d
Borteq2 <borteq2@yandex.ru>
2023-03-20 13:18:03 +0300

    add list generators add slices

--- basics/lists.py
+++ basics/lists.py
@@ -9,6 +9,13 @@ mix = ['a', 123, {'name': 'Ford'}]
 # Use a constructor
 # numbers = list(1,2,3,4) # Not common
 
+# Use a generator
+# Create a list of numbers from 1 to 14
+print([i for i in range(1, 15)])
+
+# Also you can add conditions to generators
+print([i for i in range(1, 15) if i != 5])
+
 # Get a value
 print(fruits[1])
 
@@ -46,3 +53,26 @@ print(fruits)
 # Reverse sort
 fruits.sort(reverse=True)
 print(fruits)
+
+# Slice list
+# It`s some kind of filters, but with simple syntax.
+# syntax is some_list[START:STOP:STEP]
+
+# Whole list
+print(fruits[:])
+print(fruits[::])
+
+# Odd in order
+print(fruits[::2])
+
+# Even in order
+print(fruits[1::2])
+
+# Reverse list
+print(fruits[::-1])
+
+# All from 2
+print(fruits[2:])
+
+# All before 3
+print(fruits[:3])
